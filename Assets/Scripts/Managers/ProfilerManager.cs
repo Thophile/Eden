287e5658dfbadf44a72075deb9cdb69064f0cd96
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2022-02-11 16:34:51 +0100

    Create naive algorithme

--- Assets/Scripts/Managers/ProfilerManager.cs
+++ Assets/Scripts/Managers/ProfilerManager.cs
@@ -16,7 +16,6 @@ namespace Assets.Scripts.Managers
         {
             GameManager.gameState = new GameState();
             isPaused = false;
-            StartCoroutine(nameof(UpdateAnts));
         }
 
         void Update()
@@ -25,8 +24,13 @@ namespace Assets.Scripts.Managers
             GameManager.gameState.gameTime += dt;
             if (GameManager.gameState.gameTime < 2f) return;
             if(dt < 0.034f){
-                frames.Add((activeAnts.Count, dt));
+                int count = activeAnts.Count;
 
+                frames.Add((count, dt));
+                for (int i = 0; i < count; i++)
+                {
+                    activeAnts[i].UpdateSelf();
+                }
 
                 pheroDecayTimer += dt;
                 if (pheroDecayTimer > pheroDecayDelay)
@@ -48,31 +52,6 @@ namespace Assets.Scripts.Managers
                 UnityEditor.EditorApplication.isPlaying = false;
             }
             
-        }
-
-        public new IEnumerator UpdateAnts()
-        {
-            Stopwatch watch = new Stopwatch();
-            int MAX_MILLIS = 3;
-            watch.Start();
-            for (int i = 0; ; i++)
-            {
-                if (watch.ElapsedMilliseconds > MAX_MILLIS)
-                {
-                    watch.Reset();
-                    yield return null;
-                    watch.Start();
-                }
-                if (i > activeAnts.Count - 1)
-                {
-                    i = -1;
-                }
-                else if (activeAnts[i] != null)
-                {
-                    activeAnts[i].UpdateSelf();
-                }
-            }
-
         }
     }
 }
